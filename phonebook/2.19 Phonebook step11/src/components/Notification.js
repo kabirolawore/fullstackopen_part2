83b71b337b3f6bb54950ac859ffc1986685f426b
Kabir Olawore <horlaworey@gmail.com>
2022-12-12 21:13:25 +0100

    Adding 2.20 phonebook

--- phonebook/2.19 Phonebook step11/src/components/Notification.js
+++ phonebook/2.19 Phonebook step11/src/components/Notification.js
@@ -1,7 +1,7 @@
 const Notification = ({ message }) => {
   if (message === null) return null;
 
-  return <div className="error">{message}</div>;
+  return <div className="success">{message}</div>;
 };
 
 export default Notification;
